24b76386badc61e054411b8192e230c0a1a50415
MikolaiR <mikalairnn@gmail.com>
2024-05-05 17:42:38 +0300

    fix: rewrite bot token

--- app/Services/TelegramService.php
+++ app/Services/TelegramService.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Http;
 class TelegramService
 {
     public static function sendMessage($text){
-        $botToken = '7009263467:AAH1KLxF0buQ1BGxNQVZSpn1jav9VGgg3sQ';
+        $botToken = '6850300675:AAG05iqLFOneHRuapJfTd8RrInBR7vGoWEw';
         $chatId = '-4288575174';
 
         $response = Http::post("https://api.telegram.org/bot{$botToken}/sendMessage", [
