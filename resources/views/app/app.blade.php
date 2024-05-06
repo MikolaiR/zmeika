cb5670e54e8d2320b2950b19180267fa67c2d9ca
MikolaiR <mikalairnn@gmail.com>
2024-05-06 15:07:12 +0300

    fix: add nonce from yandex scripts

--- resources/views/app/app.blade.php
+++ resources/views/app/app.blade.php
@@ -42,7 +42,7 @@
     <!-- End Google Tag Manager -->
     <!-- Yandex.Metrika counter -->
     <meta name="yandex-verification" content="09b188626ad6c5d2" />
-    <script type="text/javascript" >
+    <script type="text/javascript" nonce="{{ $nonce }}">
         (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
             m[i].l=1*new Date();
             for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
