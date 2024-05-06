65b263a11009f1831318b3c36cd69dd3173a8c97
MikolaiR <mikalairnn@gmail.com>
2024-05-06 15:05:00 +0300

    feat: add yandex metric and web console

--- resources/views/app/app.blade.php
+++ resources/views/app/app.blade.php
@@ -40,6 +40,21 @@
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-NJL9KSHF');</script>
     <!-- End Google Tag Manager -->
+    <!-- Yandex.Metrika counter -->
+    <meta name="yandex-verification" content="09b188626ad6c5d2" />
+    <script type="text/javascript" >
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(97211887, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true
+        });
+    </script>
     <link rel="preload"
           href="https://fonts.googleapis.com/css2?family=Brygada+1918:wght@400;700&display=swap&display=swap" as="style"
           onload="this.onload=null;this.rel='stylesheet'">
@@ -54,6 +69,11 @@
 <noscript>
     <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NJL9KSHF"
                   height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
+<noscript>
+    <div>
+        <img src="https://mc.yandex.ru/watch/97211887" style="position:absolute; left:-9999px;" alt="" />
+    </div>
 </noscript>
 <!-- End Google Tag Manager (noscript) -->
 @if(isset($ldJsonSchema))
