8c26e5619bba0ca7696fb73efe38fbfa14a86395
MikolaiR <mikalairnn@gmail.com>
2024-06-22 17:32:52 +0300

    fix: rewrite sitemap

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -4,20 +4,23 @@
         xmlns:xhtml="http://www.w3.org/1999/xhtml">
     <url>
         <loc>https://zmeika.pro/</loc>
-        <lastmod>2024-05-06T12:00:14+01:00</lastmod>
-        <priority>1.0</priority>
-    </url>
-    <url>
-        <loc>https://zmeika.pro/catalog</loc>
-        <lastmod>{{ isset($lastUpdateProduct->updated_at) ?
-            $lastUpdateProduct->updated_at->tz('UTC')->toAtomString() : '2024-05-06T12:00:14+01:00' }}
+        <lastmod>
+            {{ isset($lastUpdateProduct->updated_at) ?
+            $lastUpdateProduct->updated_at->tz('UTC')->toAtomString() : '2024-06-22T12:00:14+01:00' }}
         </lastmod>
         <priority>0.9</priority>
     </url>
     <url>
-        <loc>https://zmeika.pro/news</loc>
-        <lastmod>{{ isset($lastUpdateNews->updated_at) ?
-            $lastUpdateNews->updated_at->tz('UTC')->toAtomString() : '2024-05-06T12:00:14+01:00' }}
+        <loc>https://zmeika.pro/catalog</loc>
+        <lastmod>{{ isset($lastUpdateProduct->updated_at) ?
+            $lastUpdateProduct->updated_at->tz('UTC')->toAtomString() : '2024-06-22T12:00:14+01:00' }}
+        </lastmod>
+        <priority>0.9</priority>
+    </url>
+    <url>
+        <loc>https://zmeika.pro/contacts</loc>
+        <lastmod>{{ isset($lastUpdateProduct->updated_at) ?
+            $lastUpdateProduct->updated_at->tz('UTC')->toAtomString() : '2024-06-22T12:00:14+01:00' }}
         </lastmod>
         <priority>0.8</priority>
     </url>
@@ -27,17 +30,7 @@
                 <loc>{{ route('catalog.detail', ['slug' => $product->slug])}}</loc>
                 <lastmod>{{ $product->updated_at->tz('UTC')->toAtomString() }}</lastmod>
                 <changefreq>weekly</changefreq>
-                <priority>0.8</priority>
-            </url>
-        @endforeach
-    @endif
-    @if(isset($news[0]->title))
-        @foreach ($news as $new)
-            <url>
-                <loc>{{ route('news.detail', ['slug' => $new->slug])}}</loc>
-                <lastmod>{{ $new->updated_at->tz('UTC')->toAtomString() }}</lastmod>
-                <changefreq>weekly</changefreq>
-                <priority>0.8</priority>
+                <priority>0.9</priority>
             </url>
         @endforeach
     @endif
